2dac890c583473bbb1a3ce4e763a5b5c6bbccb94
anyone nobodyson <126599430+anyoneson@users.noreply.github.com>
2025-04-30 11:23:04 -0300

    Add files via upload

--- damage_meter/damage_meter.lua
+++ damage_meter/damage_meter.lua
@@ -19,22 +19,162 @@ local lastUpdate = 0
 local updateInterval = 1 -- seconds
 local combatStartTime = 0
 
+-- Create main window
+local mainWindow = Window:new("DamageMeterWindow")
+mainWindow:SetMovable(true)
+mainWindow:SetFrameStrata("HIGH")
+mainWindow:SetToplevel(true)
+mainWindow:SetWidth(300)
+mainWindow:SetHeight(400)
+
+-- Create title label
+local titleLabel = Label:new("TitleLabel", mainWindow)
+titleLabel:SetText("Damage Meter")
+titleLabel:SetPoint("TOPLEFT", mainWindow, "TOPLEFT", 10, 10)
+titleLabel:SetWidth(280)
+titleLabel:SetHeight(30)
+titleLabel:SetFont("ChatFontNormal")
+
+-- Create damage list
+local damageList = ListBox:new("DamageList", mainWindow)
+damageList:SetPoint("TOPLEFT", titleLabel, "BOTTOMLEFT", 0, 10)
+damageList:SetWidth(280)
+damageList:SetHeight(320)
+damageList:SetFont("ChatFontNormal")
+
+-- Create settings button
+local settingsButton = Button:new("SettingsButton", mainWindow)
+settingsButton:SetText("Settings")
+settingsButton:SetPoint("BOTTOMLEFT", mainWindow, "BOTTOMLEFT", 10, 10)
+settingsButton:SetWidth(80)
+settingsButton:SetHeight(20)
+settingsButton:SetFont("ChatFontNormal")
+
+-- Create reset button
+local resetButton = Button:new("ResetButton", mainWindow)
+resetButton:SetText("Reset")
+resetButton:SetPoint("LEFT", settingsButton, "RIGHT", 10, 0)
+resetButton:SetWidth(80)
+resetButton:SetHeight(20)
+resetButton:SetFont("ChatFontNormal")
+
+-- Create settings window
+local settingsWindow = Window:new("DamageMeterSettingsWindow")
+settingsWindow:SetMovable(true)
+settingsWindow:SetFrameStrata("HIGH")
+settingsWindow:SetToplevel(true)
+settingsWindow:SetWidth(300)
+settingsWindow:SetHeight(250)
+settingsWindow:Hide()
+
+-- Create settings title label
+local settingsTitleLabel = Label:new("SettingsTitleLabel", settingsWindow)
+settingsTitleLabel:SetText("Damage Meter Settings")
+settingsTitleLabel:SetPoint("TOPLEFT", settingsWindow, "TOPLEFT", 10, 10)
+settingsTitleLabel:SetWidth(280)
+settingsTitleLabel:SetHeight(30)
+settingsTitleLabel:SetFont("ChatFontNormal")
+
+-- Create show healing checkbox
+local showHealingLabel = Label:new("ShowHealingLabel", settingsWindow)
+showHealingLabel:SetText("Show Healing:")
+showHealingLabel:SetPoint("TOPLEFT", settingsTitleLabel, "BOTTOMLEFT", 0, 20)
+showHealingLabel:SetWidth(120)
+showHealingLabel:SetHeight(20)
+showHealingLabel:SetFont("ChatFontNormal")
+
+local showHealingCheckbox = CheckBox:new("ShowHealingCheckbox", settingsWindow)
+showHealingCheckbox:SetPoint("LEFT", showHealingLabel, "RIGHT", 10, 0)
+showHealingCheckbox:SetWidth(20)
+showHealingCheckbox:SetHeight(20)
+
+-- Create time window edit
+local timeWindowLabel = Label:new("TimeWindowLabel", settingsWindow)
+timeWindowLabel:SetText("Time Window (min):")
+timeWindowLabel:SetPoint("TOPLEFT", showHealingLabel, "BOTTOMLEFT", 0, 10)
+timeWindowLabel:SetWidth(120)
+timeWindowLabel:SetHeight(20)
+timeWindowLabel:SetFont("ChatFontNormal")
+
+local timeWindowEdit = EditBox:new("TimeWindowEdit", settingsWindow)
+timeWindowEdit:SetPoint("LEFT", timeWindowLabel, "RIGHT", 10, 0)
+timeWindowEdit:SetWidth(50)
+timeWindowEdit:SetHeight(20)
+timeWindowEdit:SetFont("ChatFontNormal")
+
+-- Create target name edit
+local targetNameLabel = Label:new("TargetNameLabel", settingsWindow)
+targetNameLabel:SetText("Target Name:")
+targetNameLabel:SetPoint("TOPLEFT", timeWindowLabel, "BOTTOMLEFT", 0, 10)
+targetNameLabel:SetWidth(120)
+targetNameLabel:SetHeight(20)
+targetNameLabel:SetFont("ChatFontNormal")
+
+local targetNameEdit = EditBox:new("TargetNameEdit", settingsWindow)
+targetNameEdit:SetPoint("LEFT", targetNameLabel, "RIGHT", 10, 0)
+targetNameEdit:SetWidth(120)
+targetNameEdit:SetHeight(20)
+targetNameEdit:SetFont("ChatFontNormal")
+
+-- Create opacity slider
+local opacityLabel = Label:new("OpacityLabel", settingsWindow)
+opacityLabel:SetText("Window Opacity:")
+opacityLabel:SetPoint("TOPLEFT", targetNameLabel, "BOTTOMLEFT", 0, 10)
+opacityLabel:SetWidth(120)
+opacityLabel:SetHeight(20)
+opacityLabel:SetFont("ChatFontNormal")
+
+local opacitySlider = Slider:new("OpacitySlider", settingsWindow)
+opacitySlider:SetPoint("LEFT", opacityLabel, "RIGHT", 10, 0)
+opacitySlider:SetWidth(120)
+opacitySlider:SetHeight(20)
+opacitySlider:SetMinMaxValues(0, 100)
+opacitySlider:SetValue(100)
+
+-- Create font size slider
+local fontSizeLabel = Label:new("FontSizeLabel", settingsWindow)
+fontSizeLabel:SetText("Font Size:")
+fontSizeLabel:SetPoint("TOPLEFT", opacityLabel, "BOTTOMLEFT", 0, 10)
+fontSizeLabel:SetWidth(120)
+fontSizeLabel:SetHeight(20)
+fontSizeLabel:SetFont("ChatFontNormal")
+
+local fontSizeSlider = Slider:new("FontSizeSlider", settingsWindow)
+fontSizeSlider:SetPoint("LEFT", fontSizeLabel, "RIGHT", 10, 0)
+fontSizeSlider:SetWidth(120)
+fontSizeSlider:SetHeight(20)
+fontSizeSlider:SetMinMaxValues(8, 20)
+fontSizeSlider:SetValue(12)
+
+-- Create save button
+local settingsSaveButton = Button:new("SettingsSaveButton", settingsWindow)
+settingsSaveButton:SetText("Save")
+settingsSaveButton:SetPoint("BOTTOMLEFT", settingsWindow, "BOTTOMLEFT", 10, 10)
+settingsSaveButton:SetWidth(80)
+settingsSaveButton:SetHeight(20)
+settingsSaveButton:SetFont("ChatFontNormal")
+
+-- Create cancel button
+local settingsCancelButton = Button:new("SettingsCancelButton", settingsWindow)
+settingsCancelButton:SetText("Cancel")
+settingsCancelButton:SetPoint("LEFT", settingsSaveButton, "RIGHT", 10, 0)
+settingsCancelButton:SetWidth(80)
+settingsCancelButton:SetHeight(20)
+settingsCancelButton:SetFont("ChatFontNormal")
+
 -- Function called when the addon is loaded
 function OnLoad()
     -- Debug log
     print("DamageMeter: Addon loaded")
     
     -- Configure main window
-    DamageMeterWindow:SetPosition(DamageMeterDB.windowPosition.x, DamageMeterDB.windowPosition.y)
-    DamageMeterWindow:SetAlpha(DamageMeterDB.opacity / 100)
-    DamageMeterWindow:Show(true)
-    
-    -- Debug log
-    print("DamageMeter: Window configured")
+    mainWindow:SetPosition(DamageMeterDB.windowPosition.x, DamageMeterDB.windowPosition.y)
+    mainWindow:SetAlpha(DamageMeterDB.opacity / 100)
+    mainWindow:Show()
     
     -- Configure font
-    TitleLabel:SetFontSize(DamageMeterDB.fontSize)
-    DamageList:SetFontSize(DamageMeterDB.fontSize)
+    titleLabel:SetFontSize(DamageMeterDB.fontSize)
+    damageList:SetFontSize(DamageMeterDB.fontSize)
     
     -- Register events
     RegisterEvent("COMBAT_LOG_EVENT")
@@ -45,20 +185,18 @@ function OnLoad()
     -- Debug log
     print("DamageMeter: Events registered")
     
-    -- Configure main window buttons
-    SettingsButton:SetScript("OnClick", OpenSettings)
-    ResetButton:SetScript("OnClick", ResetData)
+    -- Configure buttons
+    settingsButton:SetScript("OnClick", OpenSettings)
+    resetButton:SetScript("OnClick", ResetData)
+    settingsSaveButton:SetScript("OnClick", SaveSettings)
+    settingsCancelButton:SetScript("OnClick", CloseSettings)
     
     -- Configure settings window controls
-    ShowHealingCheckbox:SetChecked(DamageMeterDB.showHealing)
-    TimeWindowEdit:SetText(tostring(DamageMeterDB.timeWindow))
-    TargetNameEdit:SetText(DamageMeterDB.targetName)
-    OpacitySlider:SetValue(DamageMeterDB.opacity)
-    FontSizeSlider:SetValue(DamageMeterDB.fontSize)
-    
-    -- Configure settings window buttons
-    SettingsSaveButton:SetScript("OnClick", SaveSettings)
-    SettingsCancelButton:SetScript("OnClick", CloseSettings)
+    showHealingCheckbox:SetChecked(DamageMeterDB.showHealing)
+    timeWindowEdit:SetText(tostring(DamageMeterDB.timeWindow))
+    targetNameEdit:SetText(DamageMeterDB.targetName)
+    opacitySlider:SetValue(DamageMeterDB.opacity)
+    fontSizeSlider:SetValue(DamageMeterDB.fontSize)
     
     -- Start update timer
     SetTimer(updateInterval * 1000, UpdateDamageList)
@@ -69,26 +207,26 @@ end
 
 -- Function to open settings
 function OpenSettings()
-    DamageMeterSettingsWindow:Show(true)
+    settingsWindow:Show()
 end
 
 -- Function to close settings
 function CloseSettings()
-    DamageMeterSettingsWindow:Show(false)
+    settingsWindow:Hide()
 end
 
 -- Function to save settings
 function SaveSettings()
-    DamageMeterDB.showHealing = ShowHealingCheckbox:GetChecked()
-    DamageMeterDB.timeWindow = tonumber(TimeWindowEdit:GetText()) or 60
-    DamageMeterDB.targetName = TargetNameEdit:GetText()
-    DamageMeterDB.opacity = OpacitySlider:GetValue()
-    DamageMeterDB.fontSize = FontSizeSlider:GetValue()
+    DamageMeterDB.showHealing = showHealingCheckbox:GetChecked()
+    DamageMeterDB.timeWindow = tonumber(timeWindowEdit:GetText()) or 60
+    DamageMeterDB.targetName = targetNameEdit:GetText()
+    DamageMeterDB.opacity = opacitySlider:GetValue()
+    DamageMeterDB.fontSize = fontSizeSlider:GetValue()
     
     -- Apply settings
-    DamageMeterWindow:SetAlpha(DamageMeterDB.opacity / 100)
-    TitleLabel:SetFontSize(DamageMeterDB.fontSize)
-    DamageList:SetFontSize(DamageMeterDB.fontSize)
+    mainWindow:SetAlpha(DamageMeterDB.opacity / 100)
+    titleLabel:SetFontSize(DamageMeterDB.fontSize)
+    damageList:SetFontSize(DamageMeterDB.fontSize)
     
     CloseSettings()
 end
@@ -103,7 +241,7 @@ end
 
 -- Function to update damage list
 function UpdateDamageList()
-    DamageList:Clear()
+    damageList:Clear()
     
     -- Sort data by total damage
     local sortedData = {}
@@ -123,7 +261,7 @@ function UpdateDamageList()
     -- Add items to list
     for i, data in ipairs(sortedData) do
         local prefix = data.isHealing and "H" or "D"
-        DamageList:AddItem(string.format("%d. [%s] %s - %s", i, prefix, data.name, FormatNumber(data.total)))
+        damageList:AddItem(string.format("%d. [%s] %s - %s", i, prefix, data.name, FormatNumber(data.total)))
     end
 end
 
@@ -184,7 +322,7 @@ end
 function OnUnload()
     -- Save window position
     DamageMeterDB.windowPosition = {
-        x = DamageMeterWindow:GetLeft(),
-        y = DamageMeterWindow:GetTop()
+        x = mainWindow:GetLeft(),
+        y = mainWindow:GetTop()
     }
 end 
--- damage_meter/toc.g
+++ damage_meter/toc.g
@@ -1,8 +1,30 @@
 ## Title: Damage Meter
 ## Author: Xizde
 ## Version: 1.0
-## Notes: Medidor de dano/cura para ArcheRage
+## Notes: Damage/Heal meter for ArcheRage
 
+-- Import Objects
+ADDON:ImportObject(OBJECT_TYPE.TEXT_STYLE)
+ADDON:ImportObject(OBJECT_TYPE.BUTTON)
+ADDON:ImportObject(OBJECT_TYPE.DRAWABLE)
+ADDON:ImportObject(OBJECT_TYPE.NINE_PART_DRAWABLE)
+ADDON:ImportObject(OBJECT_TYPE.COLOR_DRAWABLE)
+ADDON:ImportObject(OBJECT_TYPE.WINDOW)
+ADDON:ImportObject(OBJECT_TYPE.LABEL)
+ADDON:ImportObject(OBJECT_TYPE.ICON_DRAWABLE)
+ADDON:ImportObject(OBJECT_TYPE.IMAGE_DRAWABLE)
+
+-- Import APIs
+ADDON:ImportAPI(API_TYPE.OPTION.id)
+ADDON:ImportAPI(API_TYPE.CHAT.id)
+ADDON:ImportAPI(API_TYPE.ACHIEVEMENT.id)
+ADDON:ImportAPI(API_TYPE.UNIT.id)
+ADDON:ImportAPI(API_TYPE.LOCALE.id)
+ADDON:ImportAPI(API_TYPE.PLAYER.id)
+ADDON:ImportAPI(API_TYPE.EQUIPMENT.id)
+ADDON:ImportAPI(API_TYPE.BAG.id)
+
+-- Load files
 apitypes.lua
 windowcommon.lua
 window.lua
